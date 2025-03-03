0f023364fdbb0b2a54699f54d4084f67d2d8c7f3
Anna Radchuk <108876851+anyaradchuk87@users.noreply.github.com>
2025-03-03 15:45:49 +0000

    16

--- index.js
+++ index.js
@@ -2,13 +2,26 @@ const bot = require("./bot");
 
 module.exports = async (req, res) => {
   try {
-    const body = req.body;
+    if (req.method === "POST") {
+      let body = "";
 
-    if (body && body.message) {
-      bot.processUpdate(body);
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+
+      req.on("end", () => {
+        const update = JSON.parse(body);
+
+        if (update.message) {
+          console.log("Отримано повідомлення:", update.message);
+          bot.processUpdate(update);
+        }
+
+        res.status(200).send("Бот отримав оновлення!");
+      });
+    } else {
+      res.status(405).send("Метод не дозволений");
     }
-
-    res.status(200).send("Бот отримав оновлення!");
   } catch (error) {
     console.error("Помилка:", error);
     res.status(500).send("Щось пішло не так...");

842f6378c976d660e8e0e2be052859e7943e4622
Anna Radchuk <108876851+anyaradchuk87@users.noreply.github.com>
2025-03-03 16:27:30 +0000

    21

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,35 @@
-// api/bot.js
-import bot from "../bot"; // Імпортуємо екземпляр бота
+import bot from "../pages/bot";
 
-export default async (req, res) => {
+const bot = require("./bot");
+
+module.exports = async (req, res) => {
   try {
     if (req.method === "POST") {
       let body = "";
 
       req.on("data", (chunk) => {
-        body += chunk.toString(); // Додаємо отриманий кусок даних
+        body += chunk.toString();
       });
 
       req.on("end", () => {
         if (body) {
           const update = JSON.parse(body);
-          console.log("📩 Отримано оновлення:", update.message);
 
           if (update.message) {
-            bot.processUpdate(update); // Обробляємо отримані оновлення
+            console.log("📩 Отримано повідомлення:", update.message);
+            bot.processUpdate(update);
           }
         }
 
-        res.status(200).send("✅ Бот отримав оновлення!"); // Відповідь після обробки
+        res.status(200).send("✅ Бот отримав оновлення!");
       });
     } else if (req.method === "GET") {
-      // Якщо GET запит
-      res.status(200).send("👋 Привіт! Це сервер твого Telegram бота 🚀"); // Відповідь на GET запит
+      res.status(200).send("👋 Привіт! Це сервер твого Telegram бота 🚀");
     } else {
-      res.status(405).send("❌ Метод не дозволений"); // Якщо інший метод
+      res.status(405).send("❌ Метод не дозволений");
     }
   } catch (error) {
     console.error("❌ Помилка:", error);
-    res.status(500).send("Щось пішло не так..."); // Відповідь у випадку помилки
+    res.status(500).send("Щось пішло не так...");
   }
 };
